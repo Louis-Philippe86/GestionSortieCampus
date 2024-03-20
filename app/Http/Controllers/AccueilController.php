26ea0c89d9decb49bdd07c39cc8bff50ec7db1d1
Louis-Philippe86 <louis.philippe.coronas@gmail.fr>
2024-03-20 15:37:54 +0100

    c

--- app/Http/Controllers/AccueilController.php
+++ app/Http/Controllers/AccueilController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Participant;
 use App\Models\Sortie;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
 class AccueilController extends Controller
@@ -18,9 +16,93 @@ class AccueilController extends Controller
         return view('pageError.notFound');
     }
 
-    public static function option(Sortie $data,Participant $user){
-        $response = [$data->id,$user->id];
-        return $response;
+    public static function option(Sortie $sortie,Participant $user) {
+
+        $otpion = [];
+//        if($user->id == $sortie->participant_id) {
+//
+//            switch ($sortie->etat_id) {
+//                //Sortie Cree
+//                case 1:
+//                    $otpion = [
+//                        '<a href="' . route('sortie.afficher', ['sortie' => $sortie]) . '">Publier</a>',
+//                        '<a href="' . route('sortie.modifier', ['sortie' => $sortie]) . '">Modifier</a>'
+//                    ];
+//                    break;
+//
+//                //Sortie Ouverte
+//                case 2:
+//                    $otpion = [
+//                        '<a href="' . route('sortie.afficher', ['sortie' => $sortie]) . '">Afficher</a>',
+//                        '<a href="' . route('sortie.formCanceled', ['sortie' => $sortie]) . '">Annuler</a>'
+//                    ];
+//                    break;
+//
+//                //Sortie Annule
+//                case 6:
+//                    $otpion = [
+//                        '<a href="' . route('sortie.afficher', ['sortie' => $sortie]) . '">Afficher</a>'
+//                    ];
+//                    break;
+//
+//                default:
+//                    return $otpion;
+//            }
+//        }else{
+//            switch ($sortie->etat_id){
+//                //sortie Ouverte
+//                case 2:
+//                    $otpion = [
+//                        '<a href="' . route('sortie.afficher', ['sortie' => $sortie]) . '">Afficher</a>',
+//                        '<a href="' . route('sortie.formCanceled', ['sortie' => $sortie]) . '">S\'inscrir</a>'
+//                    ];
+//                    break;
+//
+//                //Sortie Annule
+//                case 6:
+//                    $otpion = [
+//                        '<a href="' . route('sortie.afficher', ['sortie' => $sortie]) . '">Afficher</a>'
+//                    ];
+//                    break;
+//
+//            }
+//        }
+
+
+        switch ($sortie->etat_id){
+            case 1:
+                if($user->id == $sortie->participant_id){
+                    $otpion = [
+                        '<a href="' . route('sortie.afficher', ['sortie' => $sortie]) . '">Publier</a>',
+                        '<a href="' . route('sortie.modifier', ['sortie' => $sortie]) . '">Modifier</a>'
+                    ];
+                    break;
+                }
+
+            //Sortie Ouverte
+            case 2:
+                $otpion = [
+                    '<a href="' . route('sortie.afficher', ['sortie' => $sortie]) . '">Afficher</a>'
+                ];
+                if($user->id == $sortie->participant_id){
+                    $otpion [] = '<a href="' . route('sortie.formCanceled', ['sortie' => $sortie]) . '">Annuler</a>';
+
+                    break;
+                }else{
+                    $otpion [] = '<a href="' . route('profil.inscription', ['sortie' => $sortie]) . '">S\'inscrir</a>';
+                    $otpion [] = '<a href="' . route('profil.annulerInscritpion', ['sortie' => $sortie]) . '">Se desiter</a>';
+                    break;
+                }
+            //sortie En cours
+            case 4:
+
+            //Sortie Annule
+            case 6:
+                $otpion [] ='<a href="' . route('sortie.afficher', ['sortie' => $sortie]) . '">Afficher</a>';
+                break;
+
+        }
+        return $otpion;
     }
 
 
